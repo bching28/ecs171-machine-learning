a33d730289e14e459c60c1d54e08b872d2ff46d3
bryan <bryanching7373@gmail.com>
2018-02-10 00:46:42 -0800

    fixed batch problem in SGD and added hinge loss function

--- hw1/prob3.py
+++ hw1/prob3.py
@@ -32,6 +32,7 @@ def unison_shuffled_copies(a, b):
     return a[p], b[p]
 
 def testDataAccuracy(test_w, test_x, test_y, alpha):
+    print '\n'
     prediction = 0
     for n in range(0, np.size(test_y)):
         if (np.sign(np.matmul(test_x[n], np.transpose(test_w))) == test_y[n]):
@@ -53,7 +54,7 @@ def gradientDescent():
 
     # initializing variables
     N = 10000
-    alpha = 0.0001
+    alpha = 0.00001 # 0.00001 best alpha; should not go below 0.000001
     epoch = 0 # number of iterations
     prediction = 0
 
@@ -70,7 +71,7 @@ def gradientDescent():
         # computer gradient
         resize_yxw = np.transpose([yxw, ]*780)
         gradient = np.sum( np.divide(-gradient_yx, (1+np.exp(resize_yxw))), axis=0 ) / N
-        print 'Shape:', np.shape(gradient)
+
         train_w -= alpha * gradient
 
         for n in range(0, N):
@@ -81,9 +82,10 @@ def gradientDescent():
 
         train_accuracy = prediction / float(N) * 100
 
-        print 'Epoch:', epoch, '|| Min Function:', min_function, '|| Accuracy:', train_accuracy, '|| Gradient:', np.sum(gradient)
+        norm_gradient = np.linalg.norm(gradient)
+        print 'Epoch:', epoch, '|| Min Function:', min_function, '|| Accuracy:', train_accuracy, '|| Gradient:', np.sum(gradient), '|| Norm:', norm_gradient
 
-        if (train_accuracy > 99.5): # takes about 2000 epochs
+        if (train_accuracy > 99.0 and norm_gradient < 1): # takes about 2000 epochs
             break
 
     test_w = train_w # passing in w for test...easier to follow
@@ -100,23 +102,22 @@ def stochasticGradient():
     # initializing variables
     N = 10000
     batch = 1000
-    alpha = 0.000001
+    alpha = 0.0000001
     epoch = 0 # number of iterations
     prediction = 0
+    vary = False
 
     while (True):
         epoch += 1
         prediction = 0
 
-        '''
-        np.random.shuffle(train_x)
-        np.random.shuffle(train_y)
-        # calculating batch data
-        batch_train_x = train_x[0:batch]
-        batch_train_y = train_y[0:batch]
-        '''
-
         batch_train_x, batch_train_y = unison_shuffled_copies(train_x, train_y)
+        batch_train_x = batch_train_x[0:batch]
+        batch_train_y = batch_train_y[0:batch]
+        #print 'X:', np.shape(batch_train_x)
+        #print 'Y:', np.shape(batch_train_y)
+        #time.sleep(10)
+
         # numerator of the gradient function
         resize_y = np.transpose([batch_train_y, ]*780)
         gradient_yx = resize_y*batch_train_x
@@ -127,6 +128,81 @@ def stochasticGradient():
         # computer gradient
         resize_yxw = np.transpose([yxw, ]*780)
         gradient = np.sum( np.divide(-gradient_yx, (1+np.exp(resize_yxw))), axis=0 ) / batch
+        # Problem 3d
+        # conditions to vary the step size
+        if (vary):
+            alpha = np.power(alpha, 1.0002)
+
+        #if (alpha < 0.000001 and vary):
+        #    alpha = 0.000001
+
+        #print('%.15f' % alpha)
+
+        train_w -= alpha * gradient
+
+        for b in range(0, batch):
+            if (np.sign(np.matmul(batch_train_x[b], np.transpose(train_w))) == batch_train_y[b]):
+                prediction += 1
+            else:
+                prediction += -1
+
+        train_accuracy = prediction / float(batch) * 100
+        norm_gradient = np.linalg.norm(gradient)
+        print 'Epoch:', epoch, '|| Min Function:', min_function, '|| Accuracy:', train_accuracy, '|| Gradient:', np.sum(gradient), '|| Norm:', norm_gradient
+
+        if (train_accuracy > 98.0): # takes about 2000 epochs
+            break
+
+    test_w = train_w # passing in w for test...easier to follow
+    testDataAccuracy(test_w, test_x, test_y, alpha)
+
+def LSVM():
+    # declaring global variables
+    global train_w
+    global train_x
+    global train_y
+    global test_x
+    global test_y
+
+    N = 10000
+    batch = 1000
+    alpha = 0.00001
+    epoch = 0 # number of iterations
+    prediction = 0
+    vary = True
+
+    while (True):
+        epoch += 1
+        prediction = 0
+
+        batch_train_x, batch_train_y = unison_shuffled_copies(train_x, train_y)
+        batch_train_x = batch_train_x[0:batch]
+        batch_train_y = batch_train_y[0:batch]
+
+        hinge_loss = 0
+        for b in range(0, batch):
+            one_row_yxw = batch_train_y[b] * np.matmul(batch_train_x[b], np.transpose(train_w))
+            if (one_row_yxw < 1):
+                hinge_loss += 1-one_row_yxw
+            elif (one_row_yxw > 1):
+                hinge_loss += 0
+        hinge_loss /= batch
+
+        gradient = 0
+        for b in range(0, batch):
+            one_row_yxw = batch_train_y[b] * np.matmul(batch_train_x[b], np.transpose(train_w))
+            if (one_row_yxw < 1):
+                gradient += (-batch_train_y[b] * batch_train_x[b])
+            elif (one_row_yxw > 1):
+                gradient += 0
+        gradient /= batch
+
+        if (vary):
+            alpha = np.power(alpha, 1.00015)
+
+        #if (alpha < 0.000000001 and vary): # needs to be super low for this problem
+        #    alpha = 0.000000001
+        print('Alpha: %.20f' % alpha)
 
         train_w -= alpha * gradient
 
@@ -138,14 +214,18 @@ def stochasticGradient():
 
         train_accuracy = prediction / float(batch) * 100
 
-        print 'Epoch:', epoch, '|| Min Function:', min_function, '|| Accuracy:', train_accuracy, '|| Gradient:', np.sum(gradient)
+        norm_gradient = np.linalg.norm(gradient)
+        print 'Epoch:', epoch, '|| Min Function:', hinge_loss, '|| Accuracy:', train_accuracy, '|| Gradient:', np.sum(gradient), '|| Norm:', norm_gradient
 
-        if (train_accuracy > 99.95): # takes about 2000 epochs
+        if (train_accuracy > 98.0 and norm_gradient < 10 and epoch > 5000): # takes about 2000 epochs
             break
 
     test_w = train_w # passing in w for test...easier to follow
     testDataAccuracy(test_w, test_x, test_y, alpha)
 
+
 #gradientDescent()
 
 stochasticGradient()
+
+#LSVM()
